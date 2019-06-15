a0fec1bd2436c44382a26d79ed91924d0e7ca2b8
han chiang <yaphc@hotmail.com>
2019-06-15 17:02:52 +0800

    get and post API

--- app.py
+++ app.py
@@ -1,12 +1,49 @@
+import json
 import falcon
-from db.index import connect
+import datetime
+
+from db.index import connect_db, query, query_mutate, Queries
+
+connect_db()
+
+class JSONTranslator(object):
+    def process_request(self, req, resp):
+        if req.content_length in (None, 0):
+            return
+
+        body = req.stream.read()
+        if not body:
+            raise falcon.HTTPBadRequest('Empty request body',
+                                        'A valid JSON document is required.')
+
+        try:
+            req.context['body'] = json.loads(body.decode('utf-8'))
+
+        except (ValueError, UnicodeDecodeError):
+            raise falcon.HTTPError(falcon.HTTP_753,
+                                   'Malformed JSON',
+                                   'Could not decode the request body. The '
+                                   'JSON was incorrect or not encoded as '
+                                   'UTF-8.')
 
 class HelloWorldResource:
     def on_get(self, request, response):
         response.media = ('Hello World from Falcon Python 3.6 app with' +
                           ' Gunicorn running in a container.')
 
+class Customers:
+    def on_get(self, request, response):
+        result = query(Queries.get_customers())
+        response.media = result
+    def on_post(self, request, response):
+        req_body = request.context['body']
+        result = query_mutate(Queries.create_customer(), (req_body['name'], datetime.datetime.strptime(req_body['dob'], '%Y-%m-%d').date()))
+        response.media = 'Created'
 
-connect()
-app = falcon.API()
+
+
+app = falcon.API(middleware=[
+    JSONTranslator()
+])
 app.add_route('/', HelloWorldResource())
+app.add_route('/customers', Customers())
--- db/index.py
+++ db/index.py
@@ -1,11 +1,31 @@
 import os
+import json
+import datetime
 
 import psycopg2
+import psycopg2.extras
 from psycopg2 import pool
 
 conn_pool = None
 
-def connect():
+def json_converter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+class Queries:
+    @staticmethod
+    def get_customers():
+        return 'SELECT * FROM customers'
+    
+    @staticmethod
+    def create_customer():
+        return 'INSERT INTO customers (name, dob) VALUES(%s, %s)'
+    
+    @staticmethod
+    def get_customer_by_id():
+        pass
+
+def connect_db():
     try:
         global conn_pool
         conn_pool = psycopg2.pool.SimpleConnectionPool(
@@ -19,5 +39,48 @@ def connect():
     except (Exception, psycopg2.Error) as error :
         print ("Error while connecting to PostgreSQL", error)
 
+def get_conn():
+    return conn_pool.getconn()
+
+def return_conn(conn):
+    conn_pool.putconn(conn)
+
+def format_result_date(row):
+    new_row = row
+    new_row['dob'] = new_row['dob'].strftime('%Y-%m-%d')
+    new_row['updated_at'] = new_row['updated_at'].strftime('%Y-%m-%dT%H-%M-%S')
+    return new_row
+
+def query(querystring, params = ((),)):
+    conn = get_conn()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(querystring, params)
+    print(params)
+
+    rows = cursor.fetchall()
+    result = [dict(r) for r in rows]
+    print(result)
+    result = list(map(lambda row: format_result_date(row), result))
+    
+    if len(result) == 1:
+        return result[0]
+    else:
+        return result
+
+    cursor.close()
+    return_conn(conn)
+    return    
+
+def query_mutate(querystring, params=((),)):
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute(querystring, params)
+    print(params)
+
+    conn.commit()
+    cursor.close()
+    return_conn(conn)
 
 
+    
+    
